f0cb1787efdb58ac7130ad7a1f6248aefb6983be
Jaewook Mun <mun1103.dev@gmail.com>
2024-09-27 03:31:14 +0900

    Revert "Feat: 포인트 서비스 정책 추가 및 리팩토링 - 2 (#2)"
    This reverts commit c93704fe2da2946a464948a8ebdd639953ad17dc.

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -9,23 +9,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-import static io.hhplus.tdd.point.TransactionType.CHARGE;
-import static io.hhplus.tdd.point.TransactionType.USE;
 import static io.hhplus.tdd.user.UserValidator.validate;
-import static java.lang.System.currentTimeMillis;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
 
     /** 최대 포인트 잔고 - 100만 포인트 */
-    protected static final long MAXIMUM_POINT = 1000000;
-    private final Map<Long, Lock> userLocks = new ConcurrentHashMap<>();
+    private static final long MAXIMUM_POINT = 1000000;
+
+    private final Lock lock = new ReentrantLock();
 
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
@@ -44,18 +40,15 @@ public class PointService {
         UserValidator.validate(id);
         PointValidator.validate(amount);
 
-        Lock lock = userLocks.computeIfAbsent(id, k -> new ReentrantLock());
-        lock.lock();
-
         try {
+            lock.lock();
+
             long totalPoint = userPointTable.selectById(id).point() + amount;
             if (totalPoint > MAXIMUM_POINT)
                 throw new OutOfMaximumPointException(String.format("최대로 충전할 수 있는 포인트는 %d point 입니다.", MAXIMUM_POINT));
 
-            UserPoint userPoint = userPointTable.insertOrUpdate(id, totalPoint);
-            pointHistoryTable.insert(id, amount, CHARGE, currentTimeMillis());
+            return userPointTable.insertOrUpdate(id, totalPoint);
 
-            return userPoint;
         } finally {
             lock.unlock();
         }
@@ -65,17 +58,14 @@ public class PointService {
         UserValidator.validate(id);
         PointValidator.validate(amount);
 
-        Lock lock = userLocks.computeIfAbsent(id, k -> new ReentrantLock());
-        lock.lock();
-
         try {
+            lock.lock();
+
             long balance = userPointTable.selectById(id).point() - amount;
             if (balance < 0) throw new MinusPointException("포인트 잔고가 부족합니다.");
 
-            UserPoint userPoint = userPointTable.insertOrUpdate(id, balance);
-            pointHistoryTable.insert(id, amount, USE, currentTimeMillis());
+            return userPointTable.insertOrUpdate(id, balance);
 
-            return userPoint;
         } finally {
             lock.unlock();
         }
--- src/main/java/io/hhplus/tdd/point/PointValidator.java
+++ src/main/java/io/hhplus/tdd/point/PointValidator.java
@@ -1,16 +1,8 @@
 package io.hhplus.tdd.point;
 
-import static io.hhplus.tdd.point.PointService.MAXIMUM_POINT;
-
 public class PointValidator {
-
-    /**
-     * 포인트는 null 혹은 음수가 될 수 없으며 100만 이하의 포인트만 거래(충전/사용) 가능합니다.
-     * @param point
-     */
     public static void validate(Long point) {
         if (point == null) throw new RuntimeException("point can not be null.");
         if (point < 0) throw new RuntimeException("point can not be negative.");
-        if (point > MAXIMUM_POINT) throw new RuntimeException(String.format("you can just dael below or equal %d point at a once", MAXIMUM_POINT));
     }
 }
